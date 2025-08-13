61176da2d8393ab0380779f9caf6dc2debcaeeca
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-08-14 00:37:07 +0700

    update .gitignore to exclude uploads directory and modify database connection details

--- database/db.php
+++ database/db.php
@@ -6,9 +6,9 @@
 // define('DB', 'web_porto');
 
 define('HOST', 'localhost');
-define('SIBEUX', 'sibe5579_cbux');
-define('pass', '1NvgEHFnwvDN96');
-define('DB', 'sibe5579_cloud_music');
+define('SIBEUX', 'sibs6571_cbux'); // Ganti dengan username database hosting
+define('pass', '1NvgEHFnwvDN96'); // Ganti dengan password database hosting
+define('DB', 'sibs6571_cloud_music'); // Ganti dengan nama database hosting
 
 $db = new mysqli(HOST, SIBEUX, pass, DB);
 
